d63b75fd239635e8ad712ad0f95dc95eb230dc06
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-01-30 01:35:13 +0330

    some change

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization, Cache-Control',
   });
 
   app.useGlobalPipes(
