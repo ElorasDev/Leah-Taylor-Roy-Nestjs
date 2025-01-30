fd2ccc101a3f6b89fe56ebdb7553d69093d976fc
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-01-30 21:15:06 +0330

    some change

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://localhost:3000',
-      'https://leahtaylorroymp-development.vercel.app',
-    ],
+    origin: ['https://leahtaylorroymp-development.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization, Cache-Control',
   });
