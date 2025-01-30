162057d36cf5fbc680feec9593d8a1e0e92da103
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-01-30 17:46:34 +0330

    some change

--- src/modules/media/dto/update-media.dto.ts
+++ src/modules/media/dto/update-media.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateMediaDto } from './create-media.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateMediaDto extends PartialType(CreateMediaDto) {}
+export class UpdateMediaDto extends PartialType(CreateMediaDto) {
+  @IsOptional()
+  filename?: string;
+
+  @IsOptional()
+  path?: string;
+
+  @IsOptional()
+  mimetype?: string;
+
+  @IsOptional()
+  size?: number;
+
+  @IsOptional()
+  file_type?: 'image' | 'video' | 'document';
+
+  @IsOptional()
+  uploaded_by?: number;
+
+  @IsOptional()
+  published?: boolean;
+}
