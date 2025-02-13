5a1c1b45482feb36619dda0efe45f224d97fafe4
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-02-13 21:13:47 +0330

    some change

--- src/modules/support/support.module.ts
+++ src/modules/support/support.module.ts
@@ -3,9 +3,10 @@ import { SupportService } from './support.service';
 import { SupportController } from './support.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Support } from './entities/support.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Support])],
+  imports: [TypeOrmModule.forFeature([Support, User])],
   controllers: [SupportController],
   providers: [SupportService],
 })
